62610d612312858cc2d596ce5d1bd05cc2e7cc14
Miguel Batres <miguel@batres.co>
2020-04-25 21:12:44 -0700

    Implement New Configuration Options

--- config/domainparser.php
+++ config/domainparser.php
@@ -1,5 +1,14 @@
 <?php
 
 return [
-    //
+    'cache' > [
+        'path' => sys_get_temp_dir(),
+        'life_time' => 604800 // 7 Days
+    ],
+    'list' => [
+        'url' => 'https://publicsuffix.org/list/effective_tld_names.dat',
+        'start' => '// ===BEGIN ICANN DOMAINS===',
+        'end' => '/// ===END ICANN DOMAINS===',
+        'remove' => ['//', '!']
+    ]
 ];
